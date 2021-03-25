9dd9267043b8b1cb4344fa7b3de8e96defc47611
Ryan Zemlin <rzemlin@gmail.com>
2021-03-25 14:41:03 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -27,7 +27,9 @@ class Song
   
   def self.genre_count
     genre_count = {}
-    genre_count.each {|genre| genre_count[genre] += 1}
+    genre_count.each do 
+      |genre| genre_count[genre] += 1
+    end
     genre_count
   end
 end
